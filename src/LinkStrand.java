033fcc90bbf00c6f6624d38f6c42f72f375e7ab1
Connor Ghazaleh <cfg11@duke.edu>
2017-11-04 23:46:58 -0400

    done coding part

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -12,6 +12,8 @@ public class LinkStrand implements IDnaStrand {
 	private Node myFirst,myLast;   
 	private long mySize;
 	private int myAppends;
+	private Node myCurrent;
+	private int myIndex;
 
 	public LinkStrand() {
 		this("");
@@ -23,7 +25,6 @@ public class LinkStrand implements IDnaStrand {
 	
 	@Override
 	public long size() {
-		// TODO Auto-generated method stub
 		return mySize;
 	}
 
@@ -33,13 +34,14 @@ public class LinkStrand implements IDnaStrand {
 		myLast = new Node(source);
 		myAppends = 0;
 		mySize = source.length();
+		myIndex = 0;
+		myCurrent = myFirst;
 		
 	}
 
 	@Override
 	public IDnaStrand getInstance(String source) {
-		// TODO Auto-generated method stub
-		return null;
+		return new LinkStrand(source);
 	}
 
 	@Override
@@ -68,7 +70,6 @@ public class LinkStrand implements IDnaStrand {
 			temp = temp.next;
 		}
 		String newLast = last.toString();
-		System.out.println(newLast);
 		return newLast;
 	}
 
@@ -96,14 +97,54 @@ public class LinkStrand implements IDnaStrand {
 	
 	@Override
 	public int getAppendCount() {
-		// TODO Auto-generated method stub
 		return myAppends;
 	}
 
 	@Override
 	public char charAt(int index) {
-		// TODO Auto-generated method stub
-		return 0;
+		System.out.println("starting charAt");
+		if (index > mySize) {
+			throw new IndexOutOfBoundsException();
+		}
+		System.out.println("all good so far");
+		System.out.println("mySize: " + mySize);
+		System.out.println("myIndex: " + myIndex);
+		System.out.println("index: " + index);
+		if (index < myIndex) {
+			myIndex = 0;
+			myCurrent = myFirst;
+			System.out.println("starting while loop for index < myIndex");
+			while (myIndex<index) {
+				myIndex += myCurrent.info.length();
+				if(myIndex > index) {
+					myIndex -= myCurrent.info.length();
+					int difference = index - myIndex;
+					char[] arr = myCurrent.info.toCharArray();
+					char charAtIndex = arr[difference];
+					return charAtIndex;
+				}
+				myCurrent = myCurrent.next;
+			}
+		} else if (index >= myIndex) {
+			System.out.println("starting while loop for index >= myIndex");
+			while (myIndex<=index) {
+				System.out.println("1");
+				myIndex += myCurrent.info.length();
+				System.out.println("2");
+				if(myIndex > index) {
+					myIndex -= myCurrent.info.length();
+					System.out.println("3");
+					int difference = index - myIndex;
+					char[] arr = myCurrent.info.toCharArray();
+					System.out.println("4");
+					char charAtIndex = arr[difference];
+					System.out.println("5");
+					return charAtIndex;
+				}
+				myCurrent = myCurrent.next;
+			}
+		}	
+		return ' ';
 	}
 
 }
